5d7001699eb4fdfd70c30b6fb45ed57e93ca8b72
Anoop-S-Kumar <109204898+Anoop-S-Kumar@users.noreply.github.com>
2022-09-14 12:46:42 +0530

    usermodel commited

---
+++ api_using_model/lib/models/users.dart
@@ -0,0 +1,45 @@
+// To parse this JSON data, do
+//
+//     final users = usersFromJson(jsonString);
+
+import 'dart:convert';
+
+Users usersFromJson(String str) => Users.fromJson(json.decode(str));
+
+String usersToJson(Users data) => json.encode(data.toJson());
+
+class Users {
+  Users({
+    required this.id,
+    this.name,
+    this.username,
+    this.email,
+    this.phone,
+    this.website,
+  });
+
+  int id;
+  String? name;
+  String? username;
+  String? email;
+  String? phone;
+  String? website;
+
+  factory Users.fromJson(Map<String, dynamic> json) => Users(
+        id: json["id"],
+        name: json["name"],
+        username: json["username"],
+        email: json["email"],
+        phone: json["phone"],
+        website: json["website"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "username": username,
+        "email": email,
+        "phone": phone,
+        "website": website,
+      };
+}
